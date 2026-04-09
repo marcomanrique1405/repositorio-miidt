81e1ba349dcf3fc3180720a16ca5dc347922b3bc
marcomanrique1405 <marcoantoniomanriquecastro@gmail.com>
2026-04-09 00:19:26 -0600

    Update stats.php

--- admin/views/components/stats.php
+++ admin/views/components/stats.php
@@ -97,7 +97,6 @@ function animarContador(elemento, valorFinal, duracion = 1000) {
     actualizar();
 }
 
-// Interceptamos cuando ya se actualizan los números
 function aplicarAnimacion() {
     const tesis = document.getElementById('stat-tesis');
     const directores = document.getElementById('stat-directores');
@@ -110,6 +109,5 @@ function aplicarAnimacion() {
     animarContador(digital, parseInt(digital.textContent));
 }
 
-// Espera a que carguen los datos y luego anima
 setTimeout(aplicarAnimacion, 300);
 </script>
